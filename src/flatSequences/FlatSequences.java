f433a3e69769d65152568782b3ff86d44a3a55c8
Mats Lexell <matslexell88@gmail.com>
2017-02-21 22:14:33 +0100

    Flatseq

--- src/flatSequences/FlatSequences.java
+++ src/flatSequences/FlatSequences.java
@@ -1,53 +1,46 @@
-package flatSequences;
+package flatsequences;
 
-import java.text.DecimalFormat;
-
-
-
-public class FlatSequences {
-	double[] seq = null;
-	
-	public FlatSequences(double[] seq){
-		this.seq = seq;
-	}
-	
-	public int fl(int k){
-		if(k<2){
-			return 1;
+public class Flatsequences {
+		double[] seq = null;
+		
+		public Flatsequences(double[] seq){
+			this.seq = seq;
 		}
 		
-		int fl = fl(k-1);
-		
-		if(isFlat(k)){
-			return fl+1;
-		}else{
-			return 0;
-		}
-	}
-	
-	public boolean isFlat(int k){
-		for (int s = k-1; s >= 1; s--) {
-			if(!eq(k,s)){
-				return false;
+		public int fl(int k){
+			if(k<2){
+				return 1;
 			}
+						
+			int fl = 1 + fl(k-1);
+			
+			return Math.min(fl, flatS(k));
+			
 		}
-		return true;
-	}
-	
-	public boolean eq(int k, int s){
-
-
-		double eq = (seq[k-1] - seq[s-1]) / (k-s);
-		double round = Math.round(eq * 1000000) * 1.0 / 1000000;
 		
-		return 0 < round && round < 1;
-	}
-
-	public static void main(String[] args) {
-		double[] seq = {1.9,2.8,3.7,4.6,5.6,6.5,7.4};
+		public int flatS(int k){
+			int s = k-1;
+			for (; s >= 1; s--) {
+				if(!eq(k,s)){
+					break;
+				}
+			}
+			return k-s;
+		}
 		
-		System.out.println(new FlatSequences(seq).fl(4));
-		
-	}
+		public boolean eq(int k, int s){
 
+
+			double eq = (seq[k-1] - seq[s-1]) / (k-s);
+			double round = Math.round(eq * 1000000) * 1.0 / 1000000;
+			
+			return 0 < round && round < 1;
+		}
+
+		public static void main(String[] args) {
+			double[] seq = {1.9,2.8,3.7,4.6,5.6,6.5,7.4};
+			
+			System.out.println(new Flatsequences(seq).fl(7));
+			
+		}
 }
